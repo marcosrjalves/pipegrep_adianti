230d157edfdbaee4dadd9e349a51f86393cc7f5d
MARCOS RODRIGO JUNG ALVES <digosw@gmail.com>
2021-04-10 14:25:07 -0300

    update on config files

--- app/config/log.php
+++ app/config/log.php
@@ -2,8 +2,8 @@
 return [
     'host'  =>  "127.0.0.1",
     'port'  =>  "3306",
-    'name'  =>  "pipegre",
-    'user'  =>  "pipegre",
+    'name'  =>  "pipegrep",
+    'user'  =>  "pipegrep",
     'pass'  =>  "123456",
     'type'  =>  "mysql",
     'prep'  =>  "1"
